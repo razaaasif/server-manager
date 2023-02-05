633b9a33c01b5ff15cb10042d4e37cba404c1139
razaasif <aasifraza9123@gmail.com>
2023-02-06 00:01:43 +0530

    create project and database configuration

---
+++ src/main/java/io/server/manager/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package io.server.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+}
